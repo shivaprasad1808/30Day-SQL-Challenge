48216bd2883698da3a455664eada50323c049fb0
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2024-08-04 18:59:25 +0530

    Update Day5 SQL Challenge.sql

--- Day5 SQL Challenge.sql
+++ Day5 SQL Challenge.sql
@@ -1,6 +1,5 @@
 /*Day5*/
---Write an sql query to output sales for that month, rolling average sales per month,
-rolling average sales per each month of an year
+--Write an sql query to output sales for each month, rolling average sales per month, rolling average sales per each month of an year
  
 create table amazon_books (book_id varchar(5),	book_title varchar(50), 	unit_price int    )
 
