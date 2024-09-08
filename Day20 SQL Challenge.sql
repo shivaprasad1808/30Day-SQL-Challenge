e89d9a6716deb2021641394679024b284a3e68a2
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2024-09-08 10:56:09 +0530

    Update Day20 SQL Challenge.sql

--- Day20 SQL Challenge.sql
+++ Day20 SQL Challenge.sql
@@ -1,4 +1,3 @@
-use leetcode
 
 /*
 Write a solution to display the records with three or more rows with consecutive id's,
@@ -9,14 +8,14 @@ and the number of people is greater than or equal to 100 for each.
 create table Stadium(id int, visit_date date, no_of_people int);
 
 insert into stadium values
- (1,   '2017-01-01',  10    ), 
- (2,   '2017-01-02',  109   ), 
- (3,   '2017-01-03',  150   ), 
- (4,   '2017-01-04',  99    ), 
- (5,   '2017-01-05',  145   ), 
- (6,   '2017-01-06',  1455  ), 
- (7,   '2017-01-07',  199   ), 
- (8,   '2017-01-09',  188 	);
+ (1,   '2017-01-01',  10), 
+ (2,   '2017-01-02',  109), 
+ (3,   '2017-01-03',  150), 
+ (4,   '2017-01-04',  99), 
+ (5,   '2017-01-05',  145), 
+ (6,   '2017-01-06',  1455), 
+ (7,   '2017-01-07',  199), 
+ (8,   '2017-01-09',  188);
 
 select * from stadium;
  
