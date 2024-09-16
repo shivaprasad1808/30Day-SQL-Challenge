d84f1b371f22735ec9f1bce2da14482c3d4d6a48
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2024-09-16 13:50:12 +0530

    Update Day26 SQL Challenge.sql

--- Day26 SQL Challenge.sql
+++ Day26 SQL Challenge.sql
@@ -1,4 +1,11 @@
-use leetcode
+/*
+Query: Find the monthly retention rate of users for each account separately for Dec 2020 and Jan 2021.
+Retention rate is the percentage of active users an account retains over a given period of time.
+In this case, assume the user is retained if he/she stays with the app in any future months.
+For example, if a user was active in Dec 2020 and has activity in any future month, consider them retained for Dec. 
+You can assume all accounts are present in Dec 2020 and Jan 2021. Your output should have the account ID 
+and the Jan 2021 retention rate divided by Dec 2020 retention rate.
+*/
 
 create table sf_events(event_date date, account_id varchar(5), user_id varchar(5));
 
@@ -29,8 +36,6 @@ insert into sf_events values
 
 
 select * from sf_events order by event_date; 
-
-
 
 
 with CTE_dec as (
